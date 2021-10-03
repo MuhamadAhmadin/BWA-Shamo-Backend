564d18394866db03cb7d2c448bdd6d4cbb0823d8
Muhamad Ahmadin <ahmadinations@gmail.com>
2021-10-03 14:02:27 +0700

    api profile user

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -86,4 +86,11 @@ class UserController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success([
+            $request->user()
+        ], 'Data profile berhasil di ambil');
+    }
 }
